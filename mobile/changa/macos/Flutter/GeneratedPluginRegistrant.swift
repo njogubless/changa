242b45458be1eefcb3b74f537b8ba8399f1c506c
njogubless <pnjogubless@gmail.com>
2026-03-26 14:56:03 +0300

    chore(project dependencies): new packages and dependencies added to the project

--- mobile/changa/macos/Flutter/GeneratedPluginRegistrant.swift
+++ mobile/changa/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,16 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_secure_storage_darwin
+import connectivity_plus
+import file_selector_macos
+import flutter_secure_storage_macos
 import shared_preferences_foundation
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
